b258a926bc2016dce78b407395af8aaf3e7fadb5
hyunmoo <dirgh1234@naver.com>
2014-04-16 12:54:19 +0900

    Create index.js

---
+++ index.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+
+	$('#num').click(function(){
+		var a = Math.floor((Math.random()*10)+0);
+		//document.write(a);
+		document.getElementById("num").innerHTML = a;	
+	});
+
+});
